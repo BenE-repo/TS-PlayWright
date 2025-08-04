b16a86a7d782ccec6a573a7c0bb46d2ff6fe2a67
Psychoferret <ben.h.elliott@icloud.com>
2025-08-04 19:52:40 +0100

    25/08/04 Update

--- lib/pages/registerAccount.pom.ts
+++ lib/pages/registerAccount.pom.ts
@@ -12,6 +12,7 @@ export class RegisterAccount extends BasePom {
     public readonly password: Locator;
     public readonly confirmPassword: Locator;
     public readonly newsletterSubscribe_yes: Locator;
+    public readonly newsletterSubscribe_no: Locator;
     public readonly privacyPolicy: Locator;
     public readonly continue: Locator;
 
@@ -21,6 +22,8 @@ export class RegisterAccount extends BasePom {
     public readonly telephoneError: Locator;
     public readonly passwordError: Locator;
     public readonly passwordConfirmError: Locator;
+
+    public readonly privacyPolicyWarning: Locator;
 
     constructor(page: Page) {
         
@@ -33,40 +36,20 @@ export class RegisterAccount extends BasePom {
         this.password = page.getByRole('textbox', { name: 'Password*'})
         this.confirmPassword = page.getByRole('textbox', { name: 'Password Confirm*'})
         this.newsletterSubscribe_yes = page.getByRole('group', { name: 'Newsletter' }).getByRole('radio', { name: 'Yes' });
-        this.privacyPolicy = page.getByRole('checkbox', { name: 'I have read and agree to the Privacy Policy'});
+        this.newsletterSubscribe_no = page.getByRole('group', { name: 'Newsletter' }).getByRole('radio', { name: 'No' });
+        this.privacyPolicy = page.getByRole('checkbox', { name: 'I have read and agree to the Privacy Policy' });
         this.continue = page.getByRole('button', { name: 'Continue' });
 
         const _fieldErrorBase = `//label[text()='%s']/following-sibling::div/div[@class='text-danger']`
         this.firstNameError = page.locator(sprintf(_fieldErrorBase, 'First Name'));
         this.lastNameError = page.locator(sprintf(_fieldErrorBase, 'Last Name'));
         this.emailError = page.locator(sprintf(_fieldErrorBase, 'E-Mail'));
-        //this.emailError = this.email
         this.telephoneError = page.locator(sprintf(_fieldErrorBase, 'Telephone'));
         this.passwordError = page.locator(sprintf(_fieldErrorBase, 'Password'));
         this.passwordConfirmError = page.locator(sprintf(_fieldErrorBase, 'Password Confirm'));
 
-        // this.snapshotMask = [page.locator('#common-home'), page.locator('//footer')];
-        // this.shopByCategoryBtn = page.getByRole('button', { name: 'Shop by Category' });
-        // this.shopByCategoryMenu = page.locator('#mz-component-1626147655');
-        // this.pageUrl = this.baseURL + 'index.php?route=product/special';
-        // // The breadcrumb menu on some pages interferes means that there can be multiple links
-        // // with the same name, so have to locate by id first to make sure it's the link in 
-        // // the page header menu
-        // const _baseLocMenu = page.locator('#widget-navbar-217834');
-        // this.home = _baseLocMenu.getByRole('link', { name: 'Home' });
-        // this.special = _baseLocMenu.getByRole('link', { name: 'Special Hot'});
-        // this.blog = _baseLocMenu.getByRole('link', { name: 'Blog' });
-        // this.megaMenuBtn = page.getByRole('button', { name: 'Mega Menu' });
-        // this.megaMenuMenu = page.locator('#entry281_216475');
-        // // Trying to "Show off" by doing some string formatting instead of concatting _strStart + "foobar + _strEnd"
-        // const _mmBase = `//h3[text()[contains(.,"%s")]]/../div/ul/li/a`;
-        // this.mmCatMobile = page.locator(sprintf(_mmBase, 'Mobiles'));
-        // // The icons at the top right also match multiple times on a simple selector, so need to locate by id first again
-        // const _baseLocIcons = page.locator('#entry_217820');
-        // this.compare = _baseLocIcons.getByRole('link', { name: 'Compare' });
-        // this.wishlist = _baseLocIcons.getByRole('link', { name: 'Wishlist' });
-        // // Theres no nice aria name for the shopping cart, so ugly XPath is needed instead.
-        // this.shoppingCart = page.locator(`//div[@id='entry_217825']/a[@href='#cart-total-drawer']`);
+        // The privacy policy appears to be the only thing that uses the alert at the top of the page so locating like this.
+        this.privacyPolicyWarning = page.locator('//div[@id="account-register"]/div[contains(@class,"alert")]');
 
     }
 
--- tests/account.spec.ts
+++ tests/account.spec.ts
@@ -1,7 +1,6 @@
 import { expect } from '@playwright/test';
-import { test } from '@fixtures/pageobjects.fixture';
+import { test } from '@fixtures/base.fixture';
 import { isElementValid } from '@helpers/html5Functions';
-import { register } from 'module';
 
 test.describe('Account Register and Login', async () => {
 
@@ -12,10 +11,23 @@ test.describe('Account Register and Login', async () => {
 
   });
 
+  test('Correct Field Values on Load', async({ registerAccount }) => {
+
+    await expect(registerAccount.firstName).toHaveValue('');
+    await expect(registerAccount.lastName).toHaveValue('');
+    await expect(registerAccount.email).toHaveValue('');
+    await expect(registerAccount.telephone).toHaveValue('');
+    await expect(registerAccount.password).toHaveValue('');
+    await expect(registerAccount.confirmPassword).toHaveValue('');
+    await expect(registerAccount.newsletterSubscribe_no).toBeChecked();
+    expect(await registerAccount.privacyPolicy.isChecked()).toEqual(false);
+
+  });
+
   // N.B. Don't want to spam the test site with new accounts as I don't own it. In a real world situation
   // I would end this with logging into the new account and/or checking the DB itself for the new record
   // (then deleting the account from the DB)
-  test('Register for Account: Fully Valid', async({ page, registerAccount }) => {
+  test('Register for Account: Fully Valid', async({ registerAccount }) => {
 
     await registerAccount.firstName.fill('Tom');
     await registerAccount.lastName.fill('Hunter');
@@ -39,7 +51,7 @@ test.describe('Account Register and Login', async () => {
 
   });
 
-  test('Register for Account: Empty Form', async({ page, registerAccount }) => {
+  test('Register for Account: Empty Form', async({ registerAccount }) => {
 
     await registerAccount.continue.click();
     await expect(registerAccount.firstNameError).toBeVisible();
@@ -50,7 +62,7 @@ test.describe('Account Register and Login', async () => {
 
   });
 
-  test('Register for Account: First Name Min/Max Lengths', async({ page, registerAccount }) => {
+  test('Register for Account: First Name Min/Max Lengths', async({ registerAccount }) => {
 
     // Empty
     await registerAccount.continue.click();
@@ -70,7 +82,7 @@ test.describe('Account Register and Login', async () => {
 
   });
 
-  test('Register for Account: Last Name Min/Max Lengths', async({ page, registerAccount }) => {
+  test('Register for Account: Last Name Min/Max Lengths', async({ registerAccount }) => {
 
     // Empty
     await registerAccount.continue.click();
@@ -92,7 +104,7 @@ test.describe('Account Register and Login', async () => {
 
   // email uses the browser's built-in HTML5 validation, which could be argued doesn't need testing
   // as we have no control over it, but doesn't hurt to check...
-  test('Register for Account: Email Validity - Basic String', async({ page, registerAccount }) => {
+  test('Register for Account: Email Validity - Basic String', async({ registerAccount }) => {
 
     await registerAccount.email.fill('BasicString');
     await registerAccount.continue.click();
@@ -100,7 +112,7 @@ test.describe('Account Register and Login', async () => {
 
   });
 
-  test('Register for Account: Email Validity - No domain', async({ page, registerAccount }) => {
+  test('Register for Account: Email Validity - No domain', async({ registerAccount }) => {
 
     await registerAccount.email.fill('NoDomain@');
     await registerAccount.continue.click();
@@ -110,7 +122,7 @@ test.describe('Account Register and Login', async () => {
 
   // The built-in HTML5 validation for email doesn't seem to moan about the below on FF, Chrome or Safari
   // but the post-submit validation doesn't like it, so looking for a validation message to appear, like for the other fields
-  test('Register for Account: Email Validity - No TLD', async({ page, registerAccount }) => {
+  test('Register for Account: Email Validity - No TLD', async({ registerAccount }) => {
 
     await registerAccount.email.fill('NoTLD@example');
     await registerAccount.continue.click();
@@ -121,5 +133,47 @@ test.describe('Account Register and Login', async () => {
   /*
     NOT DONE: etc. etc. for other things that can make an email address invalid.
   */
+
+  /*
+    NOT DONE: Telephone validation will be broadly the same as for email so will ignore for the purposes of this.
+  */
+
+  /*
+    NOT DONE: Password tests for length as will be the same as done before
+  */
+  
+  test('Password/Confirm Password equality - Basic', async({ registerAccount }) => {
+
+    await registerAccount.password.fill('abcd');
+    await registerAccount.confirmPassword.fill('aaaa');
+    await registerAccount.continue.click();
+    await expect(registerAccount.passwordConfirmError).toBeVisible();
+
+  });
+
+  test('Password/Confirm Password equality - Spaces Middle', async({ registerAccount }) => {
+
+    await registerAccount.password.fill('abcd');
+    await registerAccount.confirmPassword.fill('a bcd');
+    await registerAccount.continue.click();
+    await expect(registerAccount.passwordConfirmError).toBeVisible();
+
+  });
+
+  test('Password/Confirm Password equality - Spaces End', async({ registerAccount }) => {
+
+    await registerAccount.password.fill('abcd');
+    await registerAccount.confirmPassword.fill('abcd ');
+    await registerAccount.continue.click();
+    await expect(registerAccount.passwordConfirmError).toBeVisible();
+
+  });
+
+  test('Privacy Policy Accepted', async({ registerAccount }) => {
+
+    await registerAccount.continue.click();
+    await expect(registerAccount.privacyPolicyWarning).toBeVisible();
+
+  });
 
 });
